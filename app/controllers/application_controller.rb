df397dfdc2225b99e64487e8d8f16d9674363bc6
famaurel <fannymaurel@outlook.com>
2021-10-11 11:38:30 +0200

    i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,41 @@
 class ApplicationController < ActionController::Base
+
+  before_action :redirect_subdomain
+  before_action :set_locale
+
+  def extract_locale
+    parsed_locale = params[:locale] || request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/)[0]
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+
+  def redirect_subdomain
+    if request.host == 'www.fannymaurel.com'
+      redirect_to 'https://fannymaurel.com' + request.fullpath, :status => 301
+    end
+  end
+
+  def set_locale
+    I18n.locale = extract_locale || :fr
+  end
+
+  def default_url_options(options={})
+    { :locale => I18n.locale }
+  end
+
+  def presentation
+  end
+
+  def contact
+  end
+
+  def avis
+  end
+
+  def home
+  end
+
+  def mentions
+  end
+
+  private
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
-
+  scope "(:locale)", locale: /fr|en/ do
+    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+    devise_for :users
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    root to: "pages#home"
+  end
 end
