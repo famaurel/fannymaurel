3ffd29536bfe103970666244fe440fdb69529e16
famaurel <fannymaurel@outlook.com>
2021-10-11 12:24:48 +0200

    stylesheets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,12 +33,26 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.active_storage.replace_on_assign_to_many = false
 
-  config.action_mailer.perform_caching = false
+  # Don't care if the mailer can't send.
+  config.action_mailer.delivery_method = :smtp
+  host = 'localhost:3000' #replace with your own url
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => Rails.application.credentials.config[:gmail][:username],
+    :password             => Rails.application.credentials.config[:gmail][:password],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,22 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  host = 'https://fannymaurel.com' #replace with your own url
+  config.action_mailer.default_url_options = { :host => "https://fannymaurel.com" } #=> Or https://www.example.com
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'fannymaurel.com',
+    user_name:            Rails.application.credentials.config[:gmail][:username],
+    password:             Rails.application.credentials.config[:gmail][:password],
+    authentication:       'plain',
+    enable_starttls_auto: true }
+    
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
