c101e85d1f4a6185df23faf9f0ee4841e1a2e16a
famaurel <fannymaurel@outlook.com>
2021-10-26 11:25:50 +0200

    reverse feedback order

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -3,7 +3,7 @@ class FeedbacksController < ApplicationController
   before_action :set_feedback, only: [:show, :edit, :update, :destroy]
 
   def index
-    @feedbacks = Feedback.all
+    @feedbacks = Feedback.all.reverse_order
   end
 
   def show
